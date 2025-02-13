307112c0a72933667fc168ef6625e1b06a086330
Ilyos Khurozov <ilyos@khurozov.uz>
2025-02-13 11:33:36 +0500

    added apps to install.sh

--- install.sh
+++ install.sh
@@ -11,11 +11,12 @@ sudo pacman -S --noconfirm pipewire-{alsa,jack,pulse}
 systemctl --user enable pipewire{,-pulse} wireplumber
 
 # must have
-sudo pacman -S --noconfirm hyprland xdg-desktop-portal-hyprland qt{5,6}-wayland xdg-user-dirs hyprlock hyprpaper foot fuzzel waybar bash-completion libnotify dunst wl-clipboard keepassxc bat
+sudo pacman -S --noconfirm hyprland xdg-desktop-portal-hyprland qt{5,6}-wayland xdg-user-dirs hyprlock hyprpaper foot fuzzel waybar bash-completion libnotify dunst wl-clipboard keepassxc
 paru -S --noconfirm hyprpolkitagent
 
 # needed (for scripts, etc.)
-sudo pacman -S --noconfirm firefox gtk4 breeze-gtk btop mpv yt-dlp fastfetch man-db ntfs-3g cliphist wtype slurp grim jq libqalculate syncthing lf gnumeric perl-image-exiftool ffmpegthumbnailer docx2txt catdoc odt2txt transmission-cli zbar
+sudo pacman -S --noconfirm firefox gtk4 breeze-gtk btop mpv yt-dlp fastfetch man-db ntfs-3g cliphist wtype slurp grim jq libqalculate syncthing lf bat gnumeric perl-image-exiftool ffmpegthumbnailer docx2txt catdoc odt2txt transmission-cli zbar pulsemixer
+paru -S clipboard-bin
 
 # totally extra
 sudo pacman -S --noconfirm zathura-pdf-poppler jdk{,8}-openjdk libreoffice-fresh telegram-desktop tailscale docker
