cbed4764993e6a322a1137621fb74964f0981a02
Ilyos Khurozov <ilyos@khurozov.uz>
2025-06-12 09:15:59 +0500

    fixed bugs

--- install.sh
+++ install.sh
@@ -23,8 +23,8 @@ sudo pacman -S --noconfirm hyprland xdg-desktop-portal-hyprland qt{5,6}-wayland 
 paru -S --noconfirm hyprpolkitagent hyprshade
 
 # needed (for scripts, etc.)
-sudo pacman -S --noconfirm gtk4 breeze-gtk btop mpv yt-dlp fastfetch man-db ntfs-3g cliphist wtype slurp grim jq libqalculate syncthing nsxiv chafa bat gnumeric perl-image-exiftool ffmpegthumbnailer docx2txt catdoc odt2txt transmission-cli zbar pulsemixer wev
-paru -S lf-git librewolf-bin
+sudo pacman -S --noconfirm gtk4 breeze-gtk btop mpv yt-dlp fastfetch man-db ntfs-3g cliphist wtype slurp grim jq libqalculate syncthing nsxiv chafa bat gnumeric perl-image-exiftool ffmpegthumbnailer docx2txt catdoc odt2txt transmission-cli zbar pulsemixer wev difftastic
+paru -S --noconfirm lf-git librewolf-bin
 
 # totally extra
 sudo pacman -S --noconfirm tesseract-data-{eng,rus} zathura-pdf-mupdf jdk{,8}-openjdk libreoffice-fresh telegram-desktop tailscale docker cronie brightnessctl
