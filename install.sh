8712c7625e1c594c9fd951606a889b425efd608b
Ilyos Khurozov <ilyos@khurozov.uz>
2025-07-08 08:37:15 +0500

    updated git config

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ sudo pacman -S --noconfirm hyprland xdg-desktop-portal-hyprland qt{5,6}-wayland 
 paru -S --noconfirm hyprpolkitagent
 
 # needed (for scripts, etc.)
-sudo pacman -S --noconfirm gtk4 breeze-gtk btop mpv yt-dlp fastfetch man-db ntfs-3g cliphist wtype slurp grim jq libqalculate syncthing nsxiv lf chafa bat gnumeric perl-image-exiftool ffmpegthumbnailer docx2txt catdoc odt2txt transmission-cli zbar pulsemixer wev difftastic swappy
+sudo pacman -S --noconfirm gtk4 breeze-gtk btop mpv yt-dlp fastfetch man-db ntfs-3g cliphist wtype slurp grim jq libqalculate syncthing nsxiv lf chafa bat gnumeric perl-image-exiftool ffmpegthumbnailer docx2txt catdoc odt2txt transmission-cli zbar pulsemixer wev swappy
 paru -S --noconfirm librewolf-bin
 
 # totally extra
