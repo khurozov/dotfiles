d27d1bb6ae414d98a29022318b758b0c1394417a
Ilyos Khurozov <ilyos@khurozov.uz>
2025-07-31 12:29:38 +0500

    nvim: no special keymap for new note entry, just datetime

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -17,29 +17,12 @@ vim.keymap.set({"n", "i"}, "<C-s>", "<cmd>w<cr>")
 -- windows-1251 encoding for russian characters
 vim.keymap.set("n", "<F8>", ":e ++enc=cp1251<CR>")
 
--- add new entry with datetime (notes specific)
-vim.keymap.set("n", "<Leader>n", function()
-    local row = vim.api.nvim_win_get_cursor(0)[1]
-    local notEmpty = vim.fn.getline(row) ~= ""
-    local lines = {"----", os.date("%Y-%m-%d %H:%M"), ""}
-    local from = row;
-    local to = row
-    local x = 0
-    if row == 1 then
-        from = 0
-        if notEmpty then
-            to = 0
-            table.insert(lines, "")
-            x = -1
-        else
-            to = 0
-        end
-    elseif notEmpty then
-        table.insert(lines, 1, "")
-    end
-
-    
-    vim.api.nvim_buf_set_lines(0, from, to, false, lines)
-    vim.api.nvim_win_set_cursor(0, {from + #lines + x, 0})
-    vim.cmd('startinsert')
+-- insert datetime
+vim.keymap.set({"n", "i"}, "<C-d>",  function()
+    local date = os.date("%Y-%m-%d %H:%M")
+    local current_pos = vim.api.nvim_win_get_cursor(0)
+    local line = vim.api.nvim_get_current_line()
+    local new_line = line:sub(1, current_pos[2]) .. date .. line:sub(current_pos[2] + 1)
+    vim.api.nvim_set_current_line(new_line)
+    vim.api.nvim_win_set_cursor(0, {current_pos[1], current_pos[2] + #date})
 end, { noremap = true, silent = true })
